7d1281954ead7223e5180a66c539d520436e0ca9
aurora <aurora.nardelli99@gmail.com>
2023-05-11 15:28:05 +0200

    file iniziale

---
+++ index.php
@@ -0,0 +1,39 @@
+<!--connection to database-->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<!--link collegamenti-->
+
+<!--CSS-->
+<link rel="stylesheet" href="stile.css">
+<!-- libreria jQuery-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<!--CSS di Bootstrap-->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384…" crossorigin="anonymous">
+<!--JS di Bootstrap-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-…" crossorigin="anonymous"></script>
+<!--JS di Bootbox-->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.3/bootbox.min.js" integrity="sha512-…" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!--CSS con le icone Font Awesome-->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
+
+    <title>Hompage</title>
+</head>
+<body>
+    
+<div class="container mt-5"> <!-- container con all'interno il pulsante che rimanda a create.php nella uqale viene creato il codice per inserire un nuovo post-->
+    <div class="text-container"> 
+        <a href="create.php" class="btn btn-outline-dark"> + Create a New Post</a> <!-- il tag a che fa da pulsante e coolagto alla pagina create.php-->
+    </div>
+</div>
+
+
+
+
+</body>
+</html>
