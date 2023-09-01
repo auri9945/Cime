ac35fd349c9a1fb31150db90d2bd34baba2072dc
aurora <aurora.nardelli99@gmail.com>
2023-09-01 10:30:51 +0200

    merge di auri

--- index.php
+++ index.php
@@ -10,27 +10,57 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hompage</title>
 <!--link collegamenti-->
 
-<!--CSS-->
+<!-- CSS -->
 <link rel="stylesheet" href="stile.css">
-<!-- libreria jQuery-->
+
+<!-- jQuery Library -->
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-<!--CSS di Bootstrap-->
+
+<!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384…" crossorigin="anonymous">
-<!--JS di Bootstrap-->
+
+<!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-…" crossorigin="anonymous"></script>
-<!--JS di Bootbox-->
+
+<!-- Bootbox JS -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootbox.js/5.5.3/bootbox.min.js" integrity="sha512-…" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-<!--CSS con le icone Font Awesome-->
+
+<!-- Font Awesome Icons CSS -->
 <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
-<!--Google Fonts-->
+
+<!-- Google Fonts -->
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;500;600;700&display=swap" rel="stylesheet">
-    <title>Hompage</title>
+
 </head>
+
+
+
 <body>
+    <header> <!--creazione contenitore header con all'interno la navbar - barra di navogazione-->
+        <div class="header">
+            <nav class="navbar navbar-dark bg-dark justify-content-between">
+                <div class="container">
+                    <a href="index.php"> <img src="" alt="" class="logo"> CIME</a> <!-- logo con collegamento ipertestuale con la pagina index.php-->
+                    <div class="nav_items">
+                        <ul>
+                            <li><a href="">Blog</a></li>
+                            <li><a href="">About us</a></li>
+                            <li><a href="">Contacts</a></li>
+                        </ul>
+                    </div>
+                    <div class="nav_login">
+                        <a href="" class="btn btn-light">Login</a> <!-- collegamento ipertestuale con il sistema di login-->
+                        <a href="" style="color:white;">register</a> <!-- collegamento ipertestuale con il sistema di registrazione-->
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </header>
     
 <div class="container mt-5"> <!-- container con all'interno il pulsante che rimanda a create.php nella uqale viene creato il codice per inserire un nuovo post-->
 
@@ -61,15 +91,36 @@
     <div class="row">
 
         <?php foreach($query as $q) { ?>
-            <div class="col-4 d-flex justify-content-center align-items-center">
+            <div class="col-4 d-flex justify-content-center align-items-center" >
                 <div class="card text-white bg-dark mt-5">
-                    <div class="card-body" style="width: 18rem;">
-                        <h5 class="card-title"> <?php echo $q['title']; ?></h5>
+                    <div class="card-body" style="width: 18rem;" id="card-body">
+                        <h5 class="card-title" id="card-title"> <?php echo $q['title']; ?></h5>
                         <p class="card-text">   <?php echo $q['content']; ?></p>
-                        <a href="" class="btn btn-light"> Read More <span class="text-danger">&rarr;</span></a> <!--al posto del read more andrenno i rispettivi like e dislike-->
+                        <span id="messaggio"></span>
                     </div>
                 </div>
             </div>
+            <?php 
+                // imposto lo sfondo personalizzato
+                if (isset($_SESSION["color"])) { // controllo se la variabile di sessione color è impostata
+                    $color = $_SESSION["color"];
+                    //echo "color = ".$color;
+                ?>
+                <script>
+                $(document).ready(function() {
+                    let col = "<?= $color ?>";
+                    $("#card-title").css("background-color", col);
+                });
+                </script>
+                <?php
+                }
+                else { // se la variabile di sessione color non è impostata
+                    echo "La variabile di sessione color non è impostata :-(";
+                }
+
+                // inizializzo una variabile con un messaggio per l'utente
+                $messaggio = "";
+            ?>
         <?php }?>
     </div>
 
